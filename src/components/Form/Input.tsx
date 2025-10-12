0e08fcd16b567e658642173887b35ce4270c9e8f
Avinash Kant <contact.avinashkantse@gmail.com>
2025-10-12 17:19:34 +0530

    Form

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,34 @@
+interface InputProps {
+    type: string;
+    placeholderText?: string;
+    classes?: string;
+}
+
+const Input: React.FC<InputProps> = ({
+    type,
+    placeholderText,
+    classes
+}) => {
+    return (
+
+        <div className={`relative w-full ${classes}`}>
+            <input
+                type={type}
+                id="fullname"
+                className="peer w-full h-12 px-3 text-lg border border-gray-300 rounded-md outline-none
+               focus:border-blue-600 focus:ring-1 focus:ring-blue-200 transition-all"
+            />
+            <label
+                htmlFor="fullname"
+                className="absolute left-3 top-3 text-black font-semibold text-base
+               transition-all duration-200 ease-in-out
+               peer-placeholder-shown:top-3 peer-placeholder-shown:text-gray-400 peer-placeholder-shown:text-lg
+               peer-focus:top-1 peer-focus:text-xs peer-focus:text-blue-600"
+            >
+                {placeholderText}
+            </label>
+        </div>
+    );
+};
+
+export default Input;
---
+++ src/components/Form/Select.tsx
@@ -0,0 +1,34 @@
+interface SelectProps {
+    placeholderText?: string;
+    classes?: string;
+    values: ValueProp[];
+}
+
+interface ValueProp {
+    text: string;
+    value: string | number;
+}
+const Select: React.FC<SelectProps> = ({
+    placeholderText,
+    classes,
+    values
+}) => {
+    return (
+        <div className={`flex flex-col w-full ${classes}`}>
+            <label htmlFor="">{placeholderText}</label>
+            <select className="w-full h-12 px-3 text-md border border-gray-300 rounded-md" name="" id="">
+                <option value="">Select {placeholderText}</option>
+                {
+                    values.map((value, index) => (
+                        <>
+                            <option key={index} value={value.value}>{value.text}</option>
+                        </>
+                    )
+                    )
+                }
+            </select>
+        </div>
+    );
+};
+
+export default Select;
