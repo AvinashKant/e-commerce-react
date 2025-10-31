0b3a99167e2710e55126b8244df7c82ad9c6ce44
Avinash Kant <contact.avinashkantse@gmail.com>
2025-10-31 23:27:53 +0530

    commit

--- src/pages/Issues.tsx
+++ src/pages/Issues.tsx
@@ -14,53 +14,41 @@ export default function Issues() {
 
         <>
             <Breadcrumb links={Links} />
-            <div className="">
-                <div className="m-14 p-14">
-                    <div className="border-b-1">
-                        <HugeiconsIcon
-                            icon={Home01Icon}
-                            size={24}
-                            color="#000000"
-                            strokeWidth={1.5}
-                            className="items-center"
-                        />
-                        <h1>Track bugs, plan features, and organize your work with issues</h1>
-                        <p>
-                            Use issues (also known as tickets or stories on other platforms) to collaborate on ideas, solve problems, and plan your project.
-                        </p>
-                        <div className="flex gap-4">
-                            <button className=''>
-                                New item
-                            </button>
-                            <button>
-                                Import issues
-                            </button>
+            <div className="flex flex-col justify-center items-center">
+                <div className="flex flex-col items-center gap-1 max-w-2xl">
+                    <HugeiconsIcon
+                        icon={Home01Icon}
+                        size={24}
+                        color="#000000"
+                        strokeWidth={1.5}
+                        className="items-center"
+                    />
+                    <h1>Track bugs, plan features, and organize your work with issues</h1>
+                    <p>
+                        Use issues (also known as tickets or stories on other platforms) to collaborate on ideas, solve problems, and plan your project.
+                    </p>
+                    <div className="flex gap-4">
+                        <button className=''>
+                            New item
+                        </button>
+                        <button>
+                            Import issues
+                        </button>
 
-                        </div>
                     </div>
-                    <div>
-                        <div className="items-center">
-
-                            <div>
-                                <HugeiconsIcon
-                                    icon={ArrowRight01Icon}
-                                    size={24}
-                                    color="#000000"
-                                    strokeWidth={1.5}
-                                />
-                                Using Jira for issue tracking?
-                                <a href="">
-                                    See integration options
-                                </a>
-                            </div>
-                            <div>
-                                <a href="">
-                                    See integration options
-                                </a>
-                            </div>
-
-
-
+                </div>
+                <div>
+                    <div className="flex flex-row">
+                        <div>
+                            Using Jira for issue tracking?
+                            <a href="">
+                                See integration options
+                            </a>
+                        </div>
+                        <div>
+                            <a href="">
+                                See integration options
+                            </a>
                         </div>
                     </div>
                 </div>
