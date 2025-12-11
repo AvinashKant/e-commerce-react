7e2cba206e2e4b378665c5181aa0b4537ecaef49
Avinash Kant <contact.avinashkantse@gmail.com>
2025-12-11 22:42:50 +0530

    Issues

--- src/pages/issues/Issues.tsx
+++ src/pages/issues/Issues.tsx
@@ -1,6 +1,11 @@
 import Breadcrumb from '../../components/Breadcrumb'
 import { HugeiconsIcon } from '@hugeicons/react';
 import { ArrowRight01Icon, Home01Icon } from '@hugeicons/core-free-icons';
+import { NavLink } from "react-router";
+import { useSearchParams } from 'react-router-dom';
+import VerticalDivider from '../../components/Divider/VerticialDivider';
+
+
 const Links = [
     {
         "title": "Issues",
@@ -15,15 +20,40 @@ const options = [{
 
 
 export default function Issues() {
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const currentState = searchParams.get('state');
+
     return (
         <>
             <Breadcrumb links={Links} />
             <div className='flex justify-between p-1 border-b-1 mb-3'>
 
                 <div className="flex gap-4">
-                    <a href="">Open</a>
-                    <a href="">Closed</a>
-                    <a href="">All</a>
+                    <NavLink
+                        key="open"
+                        to="/issues"
+                        className="text-sm/6 font-semibold text-gray-900"
+                    >Open
+                    </NavLink>
+                    <NavLink
+                        key="closed"
+                        to="/issues?state=closed"
+                        className={`text-sm/6 font-semibold p-1 transition-colors ${currentState === 'closed'
+                            ? 'text-gray-900 bg-gray-300 rounded-md'
+                            : 'text-gray-500 hover:text-gray-900'
+                            }`}
+                    >Closed
+                    </NavLink>
+                    <NavLink
+                        key="all"
+                        to="/issues?state=all"
+                        className={`text-sm/6 font-semibold p-1 transition-colors ${currentState === 'all'
+                            ? 'text-gray-900 bg-gray-300 rounded-md'
+                            : 'text-gray-500 hover:text-gray-900'
+                            }`}
+                    >All
+                    </NavLink>
                 </div>
                 <div className="flex gap-3">
                     <button className=''>
@@ -68,6 +98,16 @@ export default function Issues() {
                             </a>
                         </div>
                     </div>
+                </div>
+            </div>
+
+            <div className="flex h-10 items-center">
+                <div className="">
+                    Bulk edit Bulk edit
+                </div>
+                <VerticalDivider className="mx-2 border-red-500 border-r-4" />
+                <div>
+                    New item
                 </div>
             </div>
         </>
