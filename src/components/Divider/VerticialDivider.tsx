7541371a64471004cd4ce0b9f5b68df022633617
Avinash Kant <contact.avinashkantse@gmail.com>
2025-12-16 14:07:05 +0530

    Test

---
+++ src/components/Divider/VerticialDivider.tsx
@@ -0,0 +1,13 @@
+interface VerticalDividerProps {
+    className?: string;
+}
+
+const VerticalDivider: React.FC<VerticalDividerProps> = ({
+    className = ""
+}) => {
+    return (
+        <div className={`border-r h-full ${className}`}></div>
+    );
+}
+
+export default VerticalDivider;
