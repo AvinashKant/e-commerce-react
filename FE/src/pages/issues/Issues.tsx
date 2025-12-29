49eb8e3924aa47d7c411e3427984a27c86747023
Avinash Kant <contact.avinashkantse@gmail.com>
2025-12-29 23:20:22 +0530

    issues page update

--- FE/src/pages/issues/Issues.tsx
+++ FE/src/pages/issues/Issues.tsx
@@ -1,115 +1,106 @@
-import Breadcrumb from '../../components/Breadcrumb'
+import Breadcrumb from '../../ui-library/Breadcrumb';
 import { HugeiconsIcon } from '@hugeicons/react';
 import { ArrowRight01Icon, Home01Icon } from '@hugeicons/core-free-icons';
-import { NavLink } from "react-router";
+import { NavLink } from 'react-router-dom';
 import { useSearchParams } from 'react-router-dom';
-import VerticalDivider from '../../components/Divider/VerticialDivider';
-
+import VerticalDivider from '../../components/Divider/VerticialDivider'; // Corrected typo in file name
+import { Button } from '../../ui-library/avinash-react-component-library';
 
 const Links = [
-    {
-        "title": "Issues",
-        "href": "/"
-    },
+  {
+    title: 'Issues',
+    href: '/',
+  },
 ];
 
-const options = [{
-    "title": "asd1",
-    "value": "asd"
-}];
-
+const options = [
+  {
+    title: 'asd1',
+    value: 'asd',
+  },
+];
 
 export default function Issues() {
-    const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
 
-    const currentState = searchParams.get('state');
+  const currentState = searchParams.get('state');
 
-    return (
-        <>
-            <Breadcrumb links={Links} />
-            <div className='flex justify-between p-1 border-b-1 mb-3'>
-
-                <div className="flex gap-4">
-                    <NavLink
-                        key="open"
-                        to="/issues"
-                        className="text-sm/6 font-semibold text-gray-900"
-                    >Open
-                    </NavLink>
-                    <NavLink
-                        key="closed"
-                        to="/issues?state=closed"
-                        className={`text-sm/6 font-semibold p-1 transition-colors ${currentState === 'closed'
-                            ? 'text-gray-900 bg-gray-300 rounded-md'
-                            : 'text-gray-500 hover:text-gray-900'
-                            }`}
-                    >Closed
-                    </NavLink>
-                    <NavLink
-                        key="all"
-                        to="/issues?state=all"
-                        className={`text-sm/6 font-semibold p-1 transition-colors ${currentState === 'all'
-                            ? 'text-gray-900 bg-gray-300 rounded-md'
-                            : 'text-gray-500 hover:text-gray-900'
-                            }`}
-                    >All
-                    </NavLink>
-                </div>
-                <div className="flex gap-3">
-                    <button className=''>
-                        Bulk edit
-                    </button>
-                    <button className=''>
-                        New item
-                    </button>
-                </div>
-
+  return (
+    <>
+      <Breadcrumb links={Links} />
+      <div className="flex justify-between border-b  border-gray-200">
+          <div className="flex gap-4">
+            <NavLink
+              key="open"
+              to="/issues"
+              className={`text-sm/6 font-semibold transition-colors ${
+                currentState == undefined && 'border-b-2 border-blue-900 text-gray-900'
+              }`}
+            >
+              Open
+            </NavLink>
+            <NavLink
+              key="closed"
+              to="/issues?state=closed"
+              className={`text-sm/6 font-semibold transition-colors ${
+                currentState === 'closed' && 'border-b-2 border-blue-900 text-gray-900'
+              }`}
+            >
+              Closed
+            </NavLink>
+            <NavLink
+              key="all"
+              to="/issues?state=all"
+              className={`text-sm/6 font-semibold transition-colors ${
+                currentState === 'all' && 'border-b-2 border-blue-900 text-gray-900'
+              }`}
+            >
+              All
+            </NavLink>
+          </div>
+          <div className="flex gap-3">
+            <Button title="Bulk edit" />
+            <Button title="New item" variant="blue" className="text-sm" />
+          </div>
+      </div>
+      <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-col items-center gap-1 max-w-2xl ">
+          <HugeiconsIcon
+            icon={Home01Icon}
+            size={24}
+            color="#000000"
+            strokeWidth={1.5}
+            className="items-center"
+          />
+          <h1 className="text-3xl text-center font-semibold">
+            Track bugs, plan features, and organize your work with issues
+          </h1>
+          <p>
+            Use issues (also known as tickets or stories on other platforms) to collaborate on
+            ideas, solve problems, and plan your project.
+          </p>
+          <div className="flex gap-4">
+            <button className="">New item</button>
+          </div>
+        </div>
+        <div className="border-t">
+          <div className="flex flex-row">
+            <div>
+              Using Jira for issue tracking?
+              <a href="">See integration options</a>
             </div>
-            <div className="flex flex-col justify-center items-center">
-                <div className="flex flex-col items-center gap-1 max-w-2xl ">
-                    <HugeiconsIcon
-                        icon={Home01Icon}
-                        size={24}
-                        color="#000000"
-                        strokeWidth={1.5}
-                        className="items-center"
-                    />
-                    <h1 className="text-3xl text-center font-semibold">Track bugs, plan features, and organize your work with issues</h1>
-                    <p>
-                        Use issues (also known as tickets or stories on other platforms) to collaborate on ideas, solve problems, and plan your project.
-                    </p>
-                    <div className="flex gap-4">
-                        <button className=''>
-                            New item
-                        </button>
-                    </div>
-                </div>
-                <div className="border-t">
-                    <div className="flex flex-row">
-                        <div>
-                            Using Jira for issue tracking?
-                            <a href="">
-                                See integration options
-                            </a>
-                        </div>
-                        <div>
-                            <a href="">
-                                See integration options
-                            </a>
-                        </div>
-                    </div>
-                </div>
+            <div>
+              <a href="">See integration options</a>
             </div>
+          </div>
+        </div>
+      </div>
 
-            <div className="flex h-10 items-center">
-                <div className="">
-                    Bulk edit Bulk edit
-                </div>
-                <VerticalDivider className="mx-2 border-red-500 border-r-4" />
-                <div>
-                    New item
-                </div>
-            </div>
-        </>
-    )
+      <div className="flex h-10 items-center">
+        <div className="">Bulk edit Bulk edit</div>
+        <VerticalDivider className="mx-2 border-red-500 border-r-4" />
+        <div>New item</div>
+      </div>
+    </>
+  );
 }
